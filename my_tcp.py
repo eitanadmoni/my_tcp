c25367b1413b8dfc90a0e0ba350d78c1338ee5e5
eitanadmoni <eitanadmoni@mail.tau.ac.il>
2025-06-25 12:51:15 +0300

    first ethernet check

---
+++ my_tcp.py
@@ -0,0 +1,35 @@
+from scapy.all import conf
+
+
+INTERFACE = "Hyper-V Virtual Ethernet Adapter"
+MY_MAC = '00155d7d9e4d'
+MAC_LENGTH = 6
+DST_MAC_INDEX  = 0
+RAW_PACKET_DATA_INDEX = 1
+
+
+def check_ether(packet):
+    if packet == None:
+        return False
+    if packet[DST_MAC_INDEX: DST_MAC_INDEX + MAC_LENGTH].hex() == MY_MAC:
+        return True
+    return False
+
+
+def get_ether_packet():
+    while True:
+        sock = conf.L2socket(iface=INTERFACE, promisc=True) # Create the socket
+        recv = sock.recv_raw()
+        packet = recv[RAW_PACKET_DATA_INDEX]
+        if check_ether(packet):
+            return packet
+
+
+def main():
+    print("sniffing ethernet packets:")
+    ethr_packet = get_ether_packet()
+    print(f"get ethr packet: {ethr_packet}")
+
+
+if __name__ == "__main__":
+    main()
